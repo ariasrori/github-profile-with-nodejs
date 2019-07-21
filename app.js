b335a53a9f5c0154aae5825e2b03f928c36e5487
Ari Zopo <ariasrori43@gmail.com>
2019-07-21 10:59:00 +0700

    Finish refactor to module profile

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-// TODO: [X] Connect to Github API
-let https = require('https')
+let profile = require('./profile.js');
 
-const username = 'ariasrori';
-const options = {
-    hostname : 'api.github.com',
-    port : 443,
-    path : `/users/${username}`,
-    method : 'GET',
-    headers: {
-        'user-agent' : 'nodejs'
-    }
-}
-
-// TODO: [x] Read the data
-let request = https.request(options, (response) => {
-    let body = ''
-    console.log('response : '+response.statusCode);
-    response.on('data', (data) => {
-        body += data
-    })
-    response.on('end', () => {
-        // TODO: [x] Parse the data
-        // Conver String to JSON (JavaScript Object)
-        let profile = JSON.parse(body);
-        if (response.statusCode === 200) {
-
-          // TODO: [x] Print the data out
-          // console.log(profile.login + ' owns ' + profile.public_repos + '');
-          console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} followers(s)`);
-        } else {
-          console.log(`Profile with username '${username}' not found`);
-        }
-    })
-});
-
-request.end();
-
-request.on('error',(e) => {
-  console.log(e);
-});
+profile.get("ariasrori");
+profile.get("yofri");
+profile.get("ronishak");
