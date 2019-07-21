3227b521857a0d947efc7ee6fb732dd3ca8fd086
Ari Zopo <ariasrori43@gmail.com>
2019-07-21 11:15:18 +0700

    Finish Interface in Terminal Console

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 let profile = require('./profile.js');
 
-profile.get("ariasrori");
-profile.get("yofri");
-profile.get("ronishak");
+// cara 1
+// profile.get("ariasrori");
+// profile.get("yofri");
+// profile.get("ronishak");
+
+// cara 2
+// let profiles = ['ariasrori','yofri','ronishak']
+// profiles.map(user => {
+//     profile.get(user)
+// })
+
+// cara 3 => input user
+let profiles = process.argv.slice(2)
+profiles.map(profile.get)
